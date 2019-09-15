ff652e889f521f4fe1d3c2b0d8b68ac37d7a3b56
Afonso09 <afonsos921@gmail.com>
2019-09-15 13:28:06 -0300

    Commit 2

--- src/java/br/com/fatecpg/meuartista/Disco.java
+++ src/java/br/com/fatecpg/meuartista/Disco.java
@@ -5,5 +5,18 @@ package br.com.fatecpg.meuartista;
  * @author Rafael Teixeira Miguel
  */
 public class Disco {
+    private String  nome;
 
+    public Disco(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
 }
