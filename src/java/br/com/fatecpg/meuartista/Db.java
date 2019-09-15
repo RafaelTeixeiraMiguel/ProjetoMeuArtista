6782033a189c98a325588c931c35ec47ae3a54de
Afonso09 <afonsos921@gmail.com>
2019-09-15 13:27:44 -0300

    Commit 2

--- src/java/br/com/fatecpg/meuartista/Db.java
+++ src/java/br/com/fatecpg/meuartista/Db.java
@@ -27,4 +27,37 @@ public class Db {
         }
         return banda;
     } 
+    private static ArrayList<Musica> musica;
+        public static ArrayList<Musica> getMusica(){
+        if (musica == null){
+            musica = new ArrayList<>();
+            musica.add(new Musica("Acústico MTV"));
+            musica.add(new Musica("Ao vivo 25 anos"));
+            musica.add(new Musica("Até onde vai"));
+            musica.add(new Musica("Charlie Brown Jr"));
+            musica.add(new Musica("Charlie Brown Jr"));
+            musica.add(new Musica("Charlie Brown Jr"));
+            musica.add(new Musica("Charlie Brown Jr"));
+            musica.add(new Musica("Charlie Brown Jr"));
+        }
+     return musica;
+    }
+        
+        private static ArrayList<Disco> disco;
+        public static ArrayList<Disco> getDisco(){
+        if (disco == null){
+            disco = new ArrayList<>();
+            disco.add(new Disco("Acústico MTV"));
+            disco.add(new Disco("Ao vivo 25 anos"));
+            disco.add(new Disco("Até onde vai"));
+            disco.add(new Disco("Meteora"));
+            disco.add(new Disco("Abbey Road"));
+            disco.add(new Disco("Calango"));
+            disco.add(new Disco("Nunca tem fim"));
+            disco.add(new Disco("1000 trutas 1000 tretas"));
+        }
+     return disco;
+
+        
+        }
 }
