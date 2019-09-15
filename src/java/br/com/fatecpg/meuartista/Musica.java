8cef4e412da61dc080ee7738dbd8dbe0ab24a9d2
Afonso09 <afonsos921@gmail.com>
2019-09-15 13:27:58 -0300

    Commit 2

--- src/java/br/com/fatecpg/meuartista/Musica.java
+++ src/java/br/com/fatecpg/meuartista/Musica.java
@@ -5,5 +5,19 @@ package br.com.fatecpg.meuartista;
  * @author Rafael Teixeira Miguel
  */
 public class Musica {
+    private String nome;
 
+    public Musica(String nome) {
+        this.nome = nome;
+    }
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
 }
